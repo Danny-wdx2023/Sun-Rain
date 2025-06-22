33de204ba1907e037043a9e0a0a05a817583184a
Danny-wdx2023 <Danny_wdx2023@aliyun.com>
2025-06-22 20:11:45 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/products">Products</RouterLink>
+    <RouterLink to="/market">Market</RouterLink>
+    <RouterLink to="/contact">Contact</RouterLink>
+  </nav>
+  <RouterView></RouterView>
+</template>
---
+++ src/router/index.ts
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/views/HomeView.vue'),
+    },
+    {
+      path: '/products',
+      name: 'products',
+      component: () => import('@/views/ProductsView.vue'),
+    },
+    {
+      path: '/market',
+      name: 'market',
+      component: () => import('@/views/MarketView.vue'),
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('@/views/ContactView.vue'),
+    },
+  ],
+})
+
+export default router
